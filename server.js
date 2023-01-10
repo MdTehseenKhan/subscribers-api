c7ad454887c7ad224683de3a577788ddaa1e7c1d
Tehseen <47675253+MdTehseenKhan@users.noreply.github.com>
2023-01-10 13:31:24 +0000

    Fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import "./db.js"
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => res.redirect('/subscribers'))
+// app.get('/', (req, res) => res.redirect('/subscribers'))
 app.use('/subscribers', subscribersRouter)
 
 app.listen(PORT, () => console.log(`Listening to the port ${PORT}...`))
